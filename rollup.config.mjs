929cf4a3b8e07c85acdb53a37c975244f5d51cb8
Aoi.K <34189164+dest12217@users.noreply.github.com>
2023-08-21 07:15:16 +0000

    Add terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,24 @@
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
 /** @var import('rollup').RollupOptions */
 export default {
     input: 'src/index.ts',
-    output: {
-        name: 'rollupMinimumSetup',
-        file: 'dist/bundle.js',
-        format: 'iife',
-    },
+    output: [
+        {
+            name: 'rollupMinimumSetup',
+            file: 'dist/bundle.js',
+            format: 'iife',
+        },
+        {
+            name: 'rollupMinimumSetup',
+            file: 'dist/bundle.min.js',
+            format: 'iife',
+            plugins: [
+                terser(),
+            ],
+        }
+    ],
     plugins: [
         typescript(),
     ],
