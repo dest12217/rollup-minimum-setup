7e4444f19e9808d690bdf13e6912b1437f2da24c
Aoi.K <34189164+dest12217@users.noreply.github.com>
2023-08-21 08:19:00 +0000

    Add es/cjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json' assert { type: 'json' };
 
 /** @var import('rollup').RollupOptions */
 export default {
@@ -7,17 +8,27 @@ export default {
     output: [
         {
             name: 'rollupMinimumSetup',
-            file: 'dist/bundle.js',
+            file: pkg.browser,
             format: 'iife',
         },
         {
             name: 'rollupMinimumSetup',
-            file: 'dist/bundle.min.js',
+            file: pkg.browser.replace(/\.js$/, '.min.js'),
             format: 'iife',
             plugins: [
                 terser(),
             ],
-        }
+        },
+        {
+            name: 'rollupMinimumSetup',
+            file: pkg.module,
+            format: 'es',
+        },
+        {
+            name: 'rollupMinimumSetup',
+            file: pkg.main,
+            format: 'cjs',
+        },
     ],
     plugins: [
         typescript(),
